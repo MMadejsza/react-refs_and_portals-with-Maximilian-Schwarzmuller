82e39770fad08243f65f58f84015b7c0a0672b57
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-11-16 20:31:11 +0000

    ref to stop timer

--- dev/src/components/TimerChallenge.jsx
+++ dev/src/components/TimerChallenge.jsx
@@ -1,14 +1,39 @@
+import React, {useState, useRef} from 'react';
+
 function TimerChallenge({title, targetTime}) {
+	const timer = useRef();
+
+	const [timerStarted, setTimerStarted] = useState(false);
+	const [timerExpired, setTimerExpired] = useState(false);
+
+	function handleStart() {
+		timer.current = setTimeout(() => {
+			setTimerExpired(true);
+		}, targetTime * 1000);
+
+		setTimerStarted(true);
+	}
+
+	function handleStop() {
+		// we can't use variable as it gets reset every rendering. Outside not either as it would be shared between the instances
+		clearTimeout(timer.current);
+	}
+
 	return (
 		<section className='challenge'>
 			<h2>{title}</h2>
+			{timerExpired && <p>You lost!</p>}
 			<p className='challenge-time'>
 				{targetTime}second{targetTime > 1 ? 's' : ''}
 			</p>
 			<p>
-				<button>Start Challange</button>
+				<button onClick={timerStarted ? handleStop : handleStart}>
+					{timerStarted ? 'Stop' : 'Start'} Challenge
+				</button>
 			</p>
-			<p className='active'>Time is runnig... / Timer inactive</p>
+			<p className={timerStarted ? 'active' : undefined}>
+				{timerStarted ? 'Time is runnig...' : 'Timer inactive'}
+			</p>
 		</section>
 	);
 }
