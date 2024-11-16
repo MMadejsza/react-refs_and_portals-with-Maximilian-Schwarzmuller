069c62cb07fb22c31a0222da1153c7c7ba6071fc
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-11-16 19:19:19 +0000

    reading input via ref

--- dev/src/components/Player.jsx
+++ dev/src/components/Player.jsx
@@ -1,11 +1,33 @@
+import React, {useState, useRef} from 'react';
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	const input = useRef();
+
+	const [playerName, setPlayerName] = useState('');
+	// const [submission, setSubmission] = useState(false);
+
+	// function handleInput(e) {
+	// 	setSubmission(false);
+	// 	setPlayerName(e.target.value);
+	// }
+
+	function handleSubmit() {
+		setPlayerName(input.current.value);
+		input.current.value = '';
+	}
+
+	return (
+		<section id='player'>
+			<h2>Welcome {playerName ?? 'Unknown'}</h2>
+			<p>
+				<input
+					type='text'
+					ref={input}
+					// onChange={handleInput}
+					// value={playerName}
+				/>
+				<button onClick={handleSubmit}>Set Name</button>
+			</p>
+		</section>
+	);
 }
